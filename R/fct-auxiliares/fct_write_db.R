31f1aa0db46c6b8dd9b3a1198db4079897cc3ba9
Mikael <mikael.coletto.eng@gmail.com>
2023-07-17 06:50:10 -0300

    Adicionando alterações para uso do SQLite; Fazendo ajustes em datas para SQLite; Escrevendo e Lendo do SQLite; Adicionando parâmetro para trabalhar com csv ou sqlite; Criando função para lidar com escrita do sqlite, e função para tratar variáveis de data do sqlite;

--- R/fct-auxiliares/fct_write_db.R
+++ R/fct-auxiliares/fct_write_db.R
@@ -7,27 +7,33 @@
 
 
 ## 0.1 - Bibliotecas e scripts fontes----
-
+source(here::here("R/fct-auxiliares/fct_change_date_sql.R"))
 
 ## 1.0 - Script/Função ----
 func_write_db <- function(df_name, df, con, overwrite, index_list, debug){
   teste_interno <- F
   if(teste_interno){
-    df_name = "db-games-1"
-    df = df_selected_tags
+    df_name = "db_games_3_clean"
+    df = df_games_selTags
     con = steamdb
     overwrite = T
     index_list = ""
-    debug = T
+    debug = F
   }
+## Essa função irá converter a coluna de data se houver
+  df <- func_change_date_sql(df, debug)
   if(debug){
     # Get the database name
-    dbname <- stringr::str_extract(dbGetInfo(con)$dbname, "(?<=/)[^/]*$")
+    dbname <- stringr::str_extract(DBI::dbGetInfo(con)$dbname, "(?<=/)[^/]*$")
     dbname <- stringr::str_extract(dbname, "^[^.]*")
     print(paste0("Escrevendo df = ", df_name,  " no db = ", dbname))
     print(paste0("Sobrescrever = ", overwrite))
     print(paste0("Index list = ", index_list))
   }
-  DBI::dbRemoveTable(con, df_name)
-  dbplyr::db_copy_to(con, df_name, df, overwrite, temporary = F)
+  if(overwrite){
+    if(DBI::dbExistsTable(con, df_name)){
+      DBI::dbRemoveTable(con, df_name)
+    }
+    dbplyr::db_copy_to(con, df_name, df, overwrite, temporary = F, message = F)
+  }
 }
